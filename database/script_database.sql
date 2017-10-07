50fe99e62e64a068ffeb73d8ae8005c4b71c4741
rpao <rpao@cin.ufpe.br>
2017-10-07 14:36:26 -0300

    Versao 2
    Versao 2

---
+++ database/script_database.sql
@@ -0,0 +1,49 @@
+DROP SCHEMA `qualismap`;
+
+CREATE SCHEMA `qualismap` ;
+
+/*
+ ______________________________________________________________________
+|NOME		 	 		 |COD | SIGNIFICADO                            |
+|------------------------|----|----------------------------------------|
+|INIT_SISTEM 			 | 00 | inicializacao do sistema               |
+|MPU_INIT 				 | 01 | inicalizacao de comunicacao com MPU    |
+|MPU_SUCCESS_INIT 		 | 02 | comunicacao efetuada com sucesso       |
+|MPU_ERROR_INIT 		 | 03 | erro de comunicacao com MPU			   |
+|MPU_DATA_TOO_LONG 		 | 04 | informacao muito longa                 |
+|MPU_ADDRESS_NACK 		 | 05 | endereco de transmissao enviou um nack |
+|MPU_DATA_NACK 			 | 06 | dados de transmissao geraram um nack   |
+|MPU_UNKNOW_ERROR 		 | 07 | erro desconhecido                      |
+|MPU_TRANSMISSION_SUCCESS| 08 | transmissao bem sucedida               |
+|MPU_TRANSMISSION_ERROR	 | 09 | transmissao mal sucedida               |
+|______________________________________________________________________|
+
+Codigos de origem do log de sistema
+ ________________________________________
+|NOME		 	 	  |COD| SIGNIFICADO  |
+|----------------------------------------|
+|SETUP_LOG			  |00 |setup (geral) |
+|INIT_MPU_LOG 		  |01 |setup do MPU  |
+|TRANSMISSION_MPU_LOG |02 |loop do MPU   |
+|INIT_GPS_LOG		  |03 |setup do GPS  |
+|TRANSMISSION_GPS_LOG |04 |loop do GPS   |
+|________________________________________|
+
+*/                                                                      
+CREATE TABLE `qualismap`.`log_sistem`(
+	ID			INT NOT NULL AUTO_INCREMENT,
+	MSG			VARCHAR2(40),
+	COD_LOG		NUMBER,
+	ORIGEM		VARCHAR2(20),
+	TIME_SEND	TIMESTAMP,
+	PRIMARY KEY (LOG_ID)
+);
+
+CREATE TABLE `qualismap`.`dados_local`(
+	latitude DOUBLE PRECISION(8,6),
+	longitude DOUBLE PRECISION(8,6),
+	data_hora TIMESTAMP,
+	classificacao VARCHAR(2) NOT NULL,
+	CHECK (classificacao = 'RE' OR classificacao = 'PR' OR classificacao = 'NR'),
+	PRIMARY KEY (latitude, longitude)
+);
--- leitura_mpu/leitura_mpu.ino
+++ leitura_mpu/leitura_mpu.ino
@@ -6,79 +6,119 @@
  *-------------------*
  * GPS               *
  *10 - GPS Module TX *
- *09 - GPS Module RX *
+ *11 - GPS Module RX *
  *-------------------*
  * MPU               *
- * VCC  5V           *
- * GND GND           *
- * SCL SCL           *
- * SDA SDA           *
- * INT 2             *
+ * VCC - 3.3V        *
+ * GND - GND         *
+ * SCL - SCL         *
+ * SDA - SDA         *
+ * AD0 - GND         *
+ * INT - 2           *
  *-------------------*/
 
 #include<Wire.h>
 #include <TinyGPS.h>
 #include <SoftwareSerial.h>
 
-//Endereco I2C do MPU6050
-#define MPU 0x68 
+#define MPU 0x68        //Endereco I2C do MPU6050
+#define PWR_MGMT_1 0x6B //PWR_MGMT_1 register
+#define AWAKE 0         //Tira do sleep mode  
+#define SLEEP 1         //Coloca no sleep mode  
 
-//Variaveis para armazenar valores dos sensores
-int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-int status_transmission = 1;
+// Codigos de origem do log de sistema
+#define SETUP_LOG     0           //Define log de setup (geral)
+#define INIT_MPU_LOG 1            //Define log de setup do MPU
+#define TRANSMISSION_MPU_LOG  2   //Define log de loop do MPU
+#define INIT_GPS_LOG 3            //Define log de setup do GPS
+#define TRANSMISSION_GPS_LOG  4   //Define log de loop do GPS
+
+// Codigos de log
+#define INIT_SISTEM 0              // inicializacao do sistema
+#define MPU_INIT 1                 // inicalizacao de comunicacao com MPU
+#define MPU_SUCCESS_INIT 2         // comunicacao efetuada com sucesso
+#define MPU_ERROR_INIT 3           // erro de comunicacao com MPU
+#define MPU_DATA_TOO_LONG 4        // informacao muito longa
+#define MPU_ADDRESS_NACK 5         // endereco de transmissao enviou um nack
+#define MPU_DATA_NACK 6            // dados de transmissao geraram um nack
+#define MPU_UNKNOW_ERROR 7         // erro desconhecido
+#define MPU_TRANSMISSION_SUCCESS 8 // transmissao bem sucedida
+#define MPU_TRANSMISSION_ERROR  9  // transmissao mal sucedida
 
 SoftwareSerial mySerial(10, 11);
 TinyGPS gps;
 
-void gpsdump(TinyGPS &gps);
-void printFloat(double f, int digits = 2);
+//Funçoes do sistema
+int get_log(int error); // transformaçao do codigo de erro da MPU para o codigo de log
+void print_log(int log_id, int origem, int data_gps = 0); // imprime o log do sistema
+void imprime_mpu(int AcX,int AcY,int AcZ,int Tmp,int GyX,int GyY); // imprimir dados do sensor mpu
+
+//Variaveis para armazenar valores dos sensores
+int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int status_transmission = 1;
+int qualidade = 0;
+int error;
 
 void setup(){
+  int cont_try = 0;
+    
   Serial.begin(9600); // inicia a comunicacao serial
   
-  pinMode (13, OUTPUT); // Pin 13 tem um LED conectado (o led piscando indica que leituras estao sendo feitas)
-  digitalWrite (13, HIGH); // set o LED
+  pinMode (13, OUTPUT); // Pin 13 tem um LED conectado (piscando indica que leituras estao sendo feitas, sem piscar indica erro)
+  pinMode (11, OUTPUT); // Pin 13 tem um LED conectado (piscando indica que leituras estao sendo feitas, sem piscar indica erro)
   
-  //Inicializa o MPU-6050
-  Wire.begin();                       // inicia I2C
-  Wire.beginTransmission(MPU);        // inicia transmissao para o endereço do MPU
-  Wire.write(0x6B);                   // PWR_MGMT_1 register
-  Wire.write(0);                      // (tira do sleep mode) inicializa o MPU-6050  
-  int error = Wire.endTransmission(true); // fecha a transmissao para o endereço do MPU
-  if (error != 0){
-    status_transmission = 0;
-    Serial.print("Setup - ");
-    if (error == 1)
-      Serial.println("Erro 01: informacao muito longa.");
-    else if (error == 2)
-      Serial.println("Erro 02: recebeu um NACK do endereco de transmissao.");
-    else if (error == 3)
-      Serial.println("Erro 03: recebeu um NACK dos dados de transmissao.");
-    else if (error == 4)
-      Serial.println("Erro 04:  erro desconhecido.");
-  }else{
-    Serial.println("Setup: Transmissao finalizada");
+  digitalWrite (13, HIGH); // set o LED verde
+  digitalWrite (11, HIGH);  // reset LED vermelho
+
+  delay(2000);
+
+  //print_log(INIT_SISTEM, SETUP_LOG);
+    
+  do{   
+    //Inicializa o MPU-6050
+    //print_log(MPU_INIT, INIT_MPU_LOG);
+    
+    Wire.begin();                 // inicia I2C
+    Wire.beginTransmission(MPU);  // inicia transmissao para o endereço do MPU
+    Wire.write(PWR_MGMT_1);       // PWR_MGMT_1 register
+    Wire.write(AWAKE);            // Inicializa o MPU-6050  
+
+    error = Wire.endTransmission(true); // fecha a transmissao para o endereço do MPU
+    if (error != 0){
+      status_transmission = 0;
+      //print_log(get_mpu_log(error), INIT_MPU_LOG);
+      cont_try++;
+      digitalWrite (13, LOW);   // reset o LED verde      
+      delay(5000);
+    }else{
+       status_transmission = 1;
+    }
+  }while (status_transmission == 0 && cont_try < 5);
+    
+  if (status_transmission == 0){ // sucesso
+    //print_log(MPU_ERROR_INIT, INIT_MPU_LOG);
+    digitalWrite (13, LOW);  // reset o LED verde
+    digitalWrite (11, LOW);  // reset LED vermelho
+  }else{ //erro
+    //print_log(MPU_SUCCESS_INIT, INIT_MPU_LOG);
   }
 }
 
 void loop(){
   if(status_transmission != 0){
-    int error;
-    digitalWrite (13, LOW);         // apaga o LED no incio do Loop
-    
+       
     Wire.beginTransmission(MPU);    // transmite para MPU
     Wire.write(0x3B);               // começa com o registrador 0x3B (ACCEL_XOUT_H)
     error = Wire.endTransmission(false);    // finalisa transmissao
     if (error != 0){
-      if (error == 1)
-        Serial.println("Erro 01: informacao muito longa.");
-      else if (error == 2)
-        Serial.println("Erro 02: recebeu um NACK do endereco de transmissao.");
-      else if (error == 3)
-        Serial.println("Erro 03: recebeu um NACK dos dados de transmissao.");
-      else if (error == 4)
-        Serial.println("Erro 04:  erro desconhecido.");
+      //print_log(get_mpu_log(error), TRANSMISSION_MPU_LOG);
+      digitalWrite (11, HIGH);  // set LED vermelho
+      delay(5000);
     }else{
+      digitalWrite (11, LOW);  // reset LED vermelho
+           
+      //print_log(MPU_TRANSMISSION_SUCCESS, TRANSMISSION_MPU_LOG);
+      
       Wire.requestFrom(MPU,14,true);  //Solicita os dados do sensor
       
       //Armazena o valor dos sensores nas variaveis correspondentes
@@ -89,24 +129,51 @@ void loop(){
       GyX=Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
       GyY=Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
       GyZ=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)  
+
+      imprime_mpu(AcX,AcY,AcZ,Tmp,GyX,GyY); // imprime dados obtidos do MPU
+
+      double angle_ac =(180/3.14)*atan2(AcY,sqrt(AcX*AcX+AcZ*AcZ));
+      double angle_gy = (180/3.14)*atan2(GyY,sqrt(GyX*GyX+GyZ*GyZ));
       
-      //Imprime valores X, Y, Z do acelerometro
-      Serial.print("AcX = "); Serial.print(AcX);
-      Serial.print(" AcY = "); Serial.print(AcY);
-      Serial.print(" AcZ = "); Serial.print(AcZ);
+      Serial.print("Angulo Acelerometro (graus): "); Serial.println((int)angle_ac);
+      Serial.print("Angulo Giroscopio (graus): "); Serial.println((int)angle_gy);
       
-      //Imprime valor da temperatura -- Calcula a temperatura em graus Celsius
-      Serial.print(" Tmp = "); Serial.print(Tmp/340.00+36.53);
-      
-      //Imprime valores X, Y, Z do giroscopio
-      Serial.print(" GyX = "); Serial.print(GyX);
-      Serial.print(" GyY = "); Serial.print(GyY);
-      Serial.print(" GyZ = "); Serial.println(GyZ);
+      //apagar o led
+      digitalWrite (13, HIGH); // set o LED
+      delay(1000);
+      digitalWrite (13, LOW); // set o LED
+      delay(4000);
     }  
-    //apagar o led
-    digitalWrite (13, HIGH); // set o LED
-    delay(1000);
-    digitalWrite (13, LOW); // set o LED
-    delay(4000);
   }
+}
+
+void imprime_mpu(int AcX,int AcY,int AcZ,int Tmp,int GyX,int GyY){
+  //Imprime valores X, Y, Z do acelerometro
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" AcY = "); Serial.print(AcY);
+  Serial.print(" AcZ = "); Serial.print(AcZ);
+  
+  //Imprime valor da temperatura -- Calcula a temperatura em graus Celsius
+  Serial.print(" Tmp = "); Serial.print(Tmp/340.00+36.53);
+  
+  //Imprime valores X, Y, Z do giroscopio
+  Serial.print(" GyX = "); Serial.print(GyX);
+  Serial.print(" GyY = "); Serial.print(GyY);
+  Serial.print(" GyZ = "); Serial.println(GyZ);
+}
+
+int get_mpu_log(int error){  
+  if (error == 1){
+    return MPU_DATA_TOO_LONG; //informacao muito longa
+  }else if (error == 2){
+    return MPU_ADDRESS_NACK; //recebeu um NACK do endereco de transmissao
+  }else if (error == 3){
+    return MPU_DATA_NACK;    //recebeu um NACK dos dados de transmissao
+  }else{
+    return MPU_UNKNOW_ERROR; //erro desconhecido.
+  }
+}
+
+void print_log(int log_id, int origem, int data_gps){
+  Serial.print("LOG: "); Serial.print(data_gps); Serial.print(" - "); Serial.print(origem); Serial.print(" - "); Serial.println(log_id);
 }
